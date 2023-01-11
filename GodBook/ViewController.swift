b5de3e6fcf63b7c1ac95ee04a22a9295265807e1
ehun9376 <ehun9376@gmail.com>
2023-01-11 19:57:26 +0800

    Initial Commit

---
+++ GodBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GodBook
+//
+//  Created by yihuang on 2023/1/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
